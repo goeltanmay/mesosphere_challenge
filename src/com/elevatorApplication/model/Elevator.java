aac605acfb54246906ee4da6a40d316bbfb7278d
Tanmay goel <goeltanmay@gmail.com>
2017-02-07 19:49:17 -0500

    Considering stoppage time when we reach the requested floor.

--- src/com/elevatorApplication/model/Elevator.java
+++ src/com/elevatorApplication/model/Elevator.java
@@ -7,6 +7,7 @@ public class Elevator {
 	private int elevatorId;
 	private int currentLevel;
 	private int direction;
+	private boolean doorOpen;
 	private Queue<Integer> requestQueue;
 	
 	public int getElevatorId() {
@@ -22,6 +23,7 @@ public class Elevator {
 		this.elevatorId = elevatorId;
 		this.currentLevel = 0;
 		this.direction = 0;
+		this.doorOpen = false;
 		this.requestQueue = new LinkedBlockingQueue<>();
 	}
 	
@@ -42,12 +44,22 @@ public class Elevator {
 	}
 	
 	public void step(){
+		// if the door is open, we first close the door. This action needs one time step.
+		if (doorOpen){
+			doorOpen = false;
+			return;
+		}
+		// if the door is not open, check if there is any stop in queue, and go to that stop. else stay where you are.
 		if(requestQueue.peek() != null)
+			// if reaching the next stop, stop at the next stop and open the door
 			if (currentLevel + direction == requestQueue.peek()){
 				this.currentLevel = getNextFloor();
+				doorOpen=true;
 				direction = 0;
 			}
 			else {
+				// if target is up, direction is 1
+				// if target is down, direction is -1
 				if(requestQueue.peek() > currentLevel)
 					direction = 1;
 				else direction = -1;
